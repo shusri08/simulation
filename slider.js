3e89e0f35a18c79a3fcccfa08a055930c2d84b17
Nilesh Kumar <97959767+nileshkumar-sf@users.noreply.github.com>
2023-09-23 13:50:26 +0530

    refactor code (#1)

--- slider.js
+++ slider.js
@@ -37,10 +37,6 @@
 //     imageTag.src = images[currentImage];
 //     document.getElementById('info').innerHTML = (currentImage + 1) + '/1';
 
-
-
-
-
 // });
 
 // const videoContainer = document.getElementsByClassName('video-container')[0];
@@ -49,7 +45,7 @@
 // startButton.addEventListener('click', () => {
 //     if (startButton.innerText === 'Start Video') {
 //        addVideo();
-//     } 
+//     }
 //     else {
 //         goBack();
 //     }
@@ -68,78 +64,93 @@
 // function goBack() {
 //     videoContainer.style.display = "none";
 //     buttonsContainer.style.display = "flex";
-    // startButton.innerText = "Start Video";
+// startButton.innerText = "Start Video";
 // }
 // Array of images
 const images = [
-    'img/a.png',
-    'img/b.png',
+  { path: "img/a.png", instruction: "Click start" },
+  { path: "img/b.png", instruction: "All set!!!!" },
 ];
 
 let currentImage = 0;
 
 // Elements
-const imageTag = document.getElementById('image');
-const infoTag = document.getElementById('info');
-const nextBtn = document.getElementById('next');
-const prevBtn = document.getElementById('prev');
-const videoContainer = document.querySelector('.video-container');
-const buttonsContainer = document.querySelector('.buttons');
-const startButton = document.getElementById('start');
+const imageTag = document.getElementById("image");
+const infoTag = document.getElementById("info");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+const videoContainer = document.querySelector(".video-container");
+const buttonsContainer = document.querySelector(".buttons");
+const startButton = document.getElementById("start");
+const instructionContainer = document.querySelector(".instruction");
+
+const carouselContainer = document.querySelector(".carousel");
 
 // Initialize info tag and image source
 function updateInfo() {
-    infoTag.innerHTML = `${currentImage + 1} / ${images.length}`;
-    imageTag.src = images[currentImage];
+  infoTag.innerHTML = `${currentImage + 1} / ${images.length}`;
+  imageTag.src = images[currentImage].path;
 }
 
 // Event listener for next button
-nextBtn.addEventListener('click', () => {
-    currentImage++;
-    if (currentImage >= images.length) {
-        currentImage = 0;
-    }
-    updateInfo();
+nextBtn.addEventListener("click", () => {
+  currentImage++;
+  if (currentImage >= images.length) {
+    currentImage = 0;
+  }
+  updateInfo();
+  setInstruction();
 });
 
 // Event listener for prev button
-prevBtn.addEventListener('click', () => {
-    currentImage--;
-    if (currentImage < 0) {
-        currentImage = images.length - 1;
-    }
-    updateInfo();
+prevBtn.addEventListener("click", () => {
+  currentImage--;
+  if (currentImage < 0) {
+    currentImage = images.length - 1;
+  }
+  updateInfo();
+  setInstruction();
 });
 
 // Event listener for start button
-startButton.addEventListener('click', () => {
-    if (startButton.innerText === 'Animated Video') {
-        addVideo();
-    } else {
-        goBack();
-    }
+startButton.addEventListener("click", () => {
+  if (startButton.innerText === "Animated Video") {
+    addVideo();
+  } else {
+    goBack();
+  }
 });
 
+let videoAdded = false; // Add this global variable to track whether the video has already been added
+
 function addVideo() {
-    const video = document.createElement('video');
-    video.src = 'img/Add a subheading (4).mp4';
+  // Check if the video has already been added
+  if (!videoAdded) {
+    const video = document.createElement("video");
+    video.src = "img/Add a subheading (4).mp4";
     video.autoplay = true;
     video.controls = true;
-    buttonsContainer.style.display = 'none';
-    videoContainer.style.display = 'block';
     videoContainer.appendChild(video);
-    startButton.innerText = 'Go Back';
+
+    // Set videoAdded to true to prevent further additions
+    videoAdded = true;
+  }
+  carouselContainer.style.display = "none";
+  videoContainer.style.display = "block";
+  startButton.innerText = "Go Back";
 }
 
 function goBack() {
-    const video = videoContainer.querySelector('video');
-    video.pause();
-    videoContainer.style.display = 'none';
-    buttonsContainer.style.display = 'flex';
-    startButton.innerText = 'Animated Video';
+  const video = videoContainer.querySelector("video");
+  video.pause();
+  videoContainer.style.display = "none";
+  carouselContainer.style.display = "flex";
+  startButton.innerText = "Animated Video";
 }
 
 // Initialize the info tag and image on page load
 updateInfo();
 
-
+function setInstruction() {
+  instructionContainer.innerText = images[currentImage].instruction;
+}
